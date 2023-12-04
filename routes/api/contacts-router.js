84d7a98b2fab3ddbfbd212f6882b5d38ed8b1791
yaroslawv-nexus <124203359+yaroslawv-nexus@users.noreply.github.com>
2023-12-04 16:45:36 +0200

    1part

--- routes/api/contacts-router.js
+++ routes/api/contacts-router.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import contactsController from '../../controllers/contacts/contacts-controller.js';
-import {isEmptyBody, isValidId, authenticate} from '../../middlewares/index.js';
+import {isEmptyBody, isValidId, authenticate, upload} from '../../middlewares/index.js';
 import { contactAddSchema, contactUpdateSchema, contactFavoriteSchema } from '../../models/Contact.js';
 import validateWrapper from '../../decorators/validateWrapper.js';
+
 
 
 const contactsRouter = express.Router()
@@ -13,7 +14,9 @@ contactsRouter.get('/', contactsController.getAllContacts);
 
 contactsRouter.get('/:contactId', isValidId, contactsController.getContactById);
 
-contactsRouter.post('/', isEmptyBody,validateWrapper(contactAddSchema), contactsController.addContact);
+// якщо деккілька файлів в 1 полі upload.array("avatar", 8);
+// файли в різних полях upload.fields([{name: "avatar", maxCount: 1}]);
+contactsRouter.post('/', upload.single("avatar") , isEmptyBody,validateWrapper(contactAddSchema), contactsController.addContact);
 
 contactsRouter.put('/:contactId', isValidId, isEmptyBody, validateWrapper(contactUpdateSchema), contactsController.updateContactById);
 
