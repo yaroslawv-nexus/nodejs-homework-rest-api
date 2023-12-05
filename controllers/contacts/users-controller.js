37def23565ef2a55417909820052223dde319216
yaroslawv-nexus <124203359+yaroslawv-nexus@users.noreply.github.com>
2023-12-05 18:47:46 +0200

    task

--- controllers/contacts/users-controller.js
+++ controllers/contacts/users-controller.js
@@ -1,7 +1,12 @@
+import fs from "fs/promises";
+import path from "path";
+import Jimp from "jimp";
 import { HttpError } from "../../helpers/index.js"
 import tryCatchWrapper from "../../decorators/tryCatchWrapper.js";
 import User from "../../models/User.js";
 
+
+const avatarsPath = path.resolve("public", "avatars");
 
 const updateSubscribe = async (req, res) => {
     const { _id } = req.user;
@@ -12,6 +17,29 @@ const updateSubscribe = async (req, res) => {
     })
 }
 
+const updateAvatar = async (req, res) => {
+ 
+    const { _id } = req.user;
+    const { path: oldPath, filename } = req.file;
+    const img = await Jimp.read(oldPath)
+  .then((lenna) => {
+    return lenna
+      .resize(250, 250) // resize
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+    const newPath = path.join(avatarsPath, filename);
+    await fs.rename(oldPath, newPath);
+    const avatarURL = path.join("avatars", filename);
+    const updateUser = await User.findByIdAndUpdate(_id, {avatarURL});
+    res.status(200).json({   
+        email: updateUser.avatarURL   
+    })
+    
+}
+
 export default {
     updateSubscribe: tryCatchWrapper(updateSubscribe),
+    updateAvatar: tryCatchWrapper(updateAvatar),
 }
--- routes/api/users-router.js
+++ routes/api/users-router.js
@@ -1,6 +1,6 @@
 import express from "express";
 import usersController from "../../controllers/contacts/users-controller.js";
-import { isEmptyBody, authenticate } from '../../middlewares/index.js';
+import { isEmptyBody, authenticate, upload } from '../../middlewares/index.js';
 import validateWrapper from '../../decorators/validateWrapper.js';
 import { updateSubscribeSchema } from "../../models/User.js";
 
@@ -8,6 +8,9 @@ import { updateSubscribeSchema } from "../../models/User.js";
 
 const usersRouter = express.Router();
 
-usersRouter.patch('/', authenticate, isEmptyBody, validateWrapper(updateSubscribeSchema), usersController.updateSubscribe);
+usersRouter.use(authenticate);
+
+usersRouter.patch('/', isEmptyBody, validateWrapper(updateSubscribeSchema), usersController.updateSubscribe);
+usersRouter.patch('/avatars', upload.single("avatar"), usersController.updateAvatar);
 
 export default usersRouter;
